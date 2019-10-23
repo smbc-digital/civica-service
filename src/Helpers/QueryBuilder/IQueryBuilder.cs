b302e3da3039e9a54e1d6dbc8aebd49c6b107ef9
Elliott Humphreys <elliott.humphreys@stockport.gov.uk>
2019-10-23 14:36:00 +0100

    feat(Query Builder): WIP, needs testings

---
+++ src/Helpers/QueryBuilder/IQueryBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace civica_service.Helpers.QueryBuilder
+{
+    public interface IQueryBuilder
+    {
+        string Build();
+
+        QueryBuilder Add(string key, string value);
+    }
+}
---
+++ src/Helpers/QueryBuilder/QueryBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace civica_service.Helpers.QueryBuilder
+{
+    public class QueryBuilder : IQueryBuilder
+    {
+        private List<string> queryStrings = new List<string>();
+
+        public QueryBuilder Add(string key, string value)
+        {
+            queryStrings.Add($"{HttpUtility.UrlEncode(key)}=\"{HttpUtility.UrlEncode(value)}\"");
+
+            return this;
+        }
+
+        public string Build()
+        {
+            var baseQuery = "http://www.google.com/exampleScript.php";
+
+            var query = queryStrings.Aggregate(
+                $"{baseQuery}?",
+                (previous, current) =>
+                {
+                    return $"{previous}&{current}";
+                });
+
+            return query;
+        }
+    }
+}
