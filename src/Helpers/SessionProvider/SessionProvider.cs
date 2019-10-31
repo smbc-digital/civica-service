200171b6368b01e194c4435c41c3483bd8f20aff
Colin Lees <colin.lees@stockport.gov.uk>
2019-10-31 10:34:24 +0000

    wip

--- src/Helpers/SessionProvider/SessionProvider.cs
+++ src/Helpers/SessionProvider/SessionProvider.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using civica_service.Helpers.QueryBuilder;
 using civica_service.Helpers.SessionProvider.Models;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Caching.Distributed;
 using StockportGovUK.AspNetCore.Gateways;
 
 namespace civica_service.Helpers.SessionProvider
@@ -12,26 +13,28 @@ namespace civica_service.Helpers.SessionProvider
         private readonly IGateway _gateway;
         private readonly IQueryBuilder _queryBuilder;
         private readonly SessionConfiguration _configuration;
+        private readonly IDistributedCache _distributedCache;
 
         public SessionProvider(
             IGateway gateway, 
             IQueryBuilder queryBuilder, 
-            IOptions<SessionConfiguration> configuration)
+            IOptions<SessionConfiguration> configuration,
+            IDistributedCache distributedCache)
         {
             _gateway = gateway;
             _queryBuilder = queryBuilder;
             _configuration = configuration.Value;
+            _distributedCache = distributedCache;
         }
 
         public async Task<string> GetSessionId(string personReference)
         {
-            /**
-             * Go to redis for key -- return if real boy
-             * generate new key
-             * register person ref against session 
-             * store in redis
-             * return sessionId
-             */
+            var sessionId = _distributedCache.GetString(personReference);
+
+            if (sessionId != null)
+            {
+                return sessionId;
+            }
 
             var url = _queryBuilder
                 .Add("docid", "crmlogin")
@@ -39,7 +42,52 @@ namespace civica_service.Helpers.SessionProvider
                 .Add("password", _configuration.Password)
                 .Build();
 
-            throw new NotImplementedException();
+            var response = await _gateway.GetAsync(url);
+            var xmlResponse = await response.Content.ReadAsStringAsync();
+            var deserializedResponse = XmlParser.DeserializeXmlStringToType<SessionIdModel>(xmlResponse, "Login").Result;
+            sessionId = deserializedResponse.SessionID;
+
+            if (!deserializedResponse.ErrorCode.Text.Equals(5))
+            {
+                throw new Exception($"API login unsuccessful, check credentials. Actual response: {xmlResponse.ToString()}");
+            }
+
+            if (string.IsNullOrWhiteSpace(sessionId))
+            {
+                throw new Exception("No session id returned");
+            }
+
+            if (!await AssignPersonToSession(sessionId, personReference))
+            {
+                throw new Exception($"Could not assign person reference {personReference} to session {sessionId}");
+            }
+
+            _distributedCache.SetStringAsync(personReference, sessionId);
+
+            return sessionId;
+        }
+
+        private async Task<bool> AssignPersonToSession(string sessionId, string personReference)
+        {
+            var url = _queryBuilder
+                .Add("outputtype", "xml")
+                .Add("SessionId", sessionId)
+                .Add("docid", "cocrmper")
+                .Add("personref", personReference)
+                .Build();
+
+            var resposne = await _gateway.GetAsync(url);
+            var xmlResponse = await resposne.Content.ReadAsStringAsync();
+
+            var result = XmlParser.DeserializeXmlStringToType<SetPersonModel>(xmlResponse, "ErrorCode");
+
+            var matchingErrorCodes = new List<string>
+            {
+                "Person Reference Set",
+                "Person CouncilTaxReference Set"
+            };
+
+            return matchingErrorCodes.Contains(result.ErrorCode.Description);
         }
     }
 }
