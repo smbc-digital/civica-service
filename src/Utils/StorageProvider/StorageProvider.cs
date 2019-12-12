80362122a24b95e2d0420456efd329ee4d595279
Priya Ganger <priya.ganger@stockport.gov.uk>
2019-12-12 13:38:49 +0000

    feat(CacheProvider): add allowCaching check

--- src/Utils/StorageProvider/StorageProvider.cs
+++ src/Utils/StorageProvider/StorageProvider.cs
@@ -16,7 +16,7 @@ namespace civica_service.Utils.StorageProvider
                     services.AddStackExchangeRedisCache(options => 
                     {
                         options.Configuration = storageProviderConfiguration["Address"] ?? "127.0.0.1";
-                        options.InstanceName = storageProviderConfiguration["Name"] ?? Assembly.GetEntryAssembly().GetName().Name;
+                        options.InstanceName = storageProviderConfiguration["Name"] ?? Assembly.GetEntryAssembly()?.GetName().Name;
                     });
                     break;
                 case "None":
