3a7120784c3e3c4a319cda932415a5500f2040cb
Elena Secara <elena.secara@stockport.gov.uk>
2020-02-26 16:19:03 +0000

    fix(BenefitsSummaryResponse): add PersonName element

--- src/Services/Models/BenefitsClaimsSummaryResponse.cs
+++ src/Services/Models/BenefitsClaimsSummaryResponse.cs
@@ -7,6 +7,9 @@ namespace civica_service.Services.Models
     [XmlRoot("HBSelectDoc")]
     public class BenefitsClaimsSummaryResponse
     {
+        [XmlElement("PersonName")]
+        public PersonName PersonName { get; set; }
+
         [XmlElement("HBClaimList")]
         public BenefitsClaimList Claims { get; set; }
     }
