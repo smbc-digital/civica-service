83fcc0f26529feda10cd5e256a69ad7d2400e317
Colin Lees <colinleesa7x@gmail.com>
2020-04-24 08:32:38 +0100

    chore: clean up application and remove Newtonsoft dependancy

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,20 +1,16 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using civica_service.Helpers.QueryBuilder;
-using civica_service.Helpers.SessionProvider;
 using civica_service.Helpers.SessionProvider.Models;
-using civica_service.Services;
 using civica_service.Utils.HealthChecks;
 using civica_service.Utils.ServiceCollectionExtensions;
 using civica_service.Utils.StorageProvider;
-using civica_service.Utils.Xml;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using StockportGovUK.AspNetCore.Middleware;
 using StockportGovUK.AspNetCore.Availability;
 using StockportGovUK.AspNetCore.Availability.Middleware;
+using StockportGovUK.AspNetCore.Middleware;
 using StockportGovUK.NetStandard.Gateways;
 
 namespace civica_service
@@ -41,10 +37,9 @@ namespace civica_service
             
             services.Configure<SessionConfiguration>(Configuration.GetSection("SessionConfiguration"));
 
-            services.AddSingleton<ICivicaService, CivicaService>();
-            services.AddSingleton<ISessionProvider, SessionProvider>();
-            services.AddTransient<IQueryBuilder, QueryBuilder>();
-            services.AddSingleton<IXmlParser, XmlParser>();
+            services.RegisterHelpers();
+            services.RegisterServices();
+            services.RegisterUtils();            
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
--- src/Utils/ServiceCollectionExtensions/ServiceCollectionExtensions.cs
+++ src/Utils/ServiceCollectionExtensions/ServiceCollectionExtensions.cs
@@ -1,11 +1,33 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using civica_service.Helpers.QueryBuilder;
+using civica_service.Helpers.SessionProvider;
+using civica_service.Services;
+using civica_service.Utils.Xml;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 
 namespace civica_service.Utils.ServiceCollectionExtensions
 {
+    [ExcludeFromCodeCoverage]
     public static class ServiceCollectionExtensions
     {
+        public static void RegisterHelpers(this IServiceCollection services)
+        {
+            services.AddSingleton<ISessionProvider, SessionProvider>();
+            services.AddTransient<IQueryBuilder, QueryBuilder>();
+        }
+
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddSingleton<ICivicaService, CivicaService>();
+        }
+
+        public static void RegisterUtils(this IServiceCollection services)
+        {
+            services.AddSingleton<IXmlParser, XmlParser>();
+        }
+
         public static void AddSwagger(this IServiceCollection services)
         {
             services.AddSwaggerGen(c =>
